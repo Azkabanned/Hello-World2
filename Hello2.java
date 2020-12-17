d707b6607bdfff989c8f38bd7deeee857e93e67b
Azkabanned <41931128+Azkabanned@users.noreply.github.com>
2020-12-17 16:14:03 -0600

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,7 @@
+public class Hello2 {
+    private static Bazinga word = new Bazinga();
+
+    public static void main(String[] args) {
+        System.out.println("Hello World, " + word.getWord());
+    }
+}
